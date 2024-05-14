38d46f3be1ec62382a2d4df9be13420298b89bd2
zachary oliver <zspoliver@gmail.com>
2024-05-14 17:47:15 -0400

    adds p3 loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")    
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [x ** 2 for x in int_list]
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)    
+
